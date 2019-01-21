126192c4bdc04ca5b6e89692daae287682ef7525
Andrey Volkov <AndreyVolkovHSEBI@yandex.ru>
2019-01-21 13:47:11 +0300

    'init'

---
+++ app/src/main/java/com/andreyvolkov/socialnetworkproject/Recyclers/RecyclerNewsfeedAdapter.java
@@ -0,0 +1,22 @@
+package com.andreyvolkov.socialnetworkproject.Recyclers;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.andreyvolkov.socialnetworkproject.R;
+
+import java.util.ArrayList;
+
+public class RecyclerNewsfeedAdapter {
+
+
+
+}
